e11fd3d83c4cddd66396ce65a4c86e5bd6d34a79
Brian Flanagan <brian@elbow.ie>
2019-02-26 08:25:44 +0000

    rm structured_content that never made it to staging

--- db/migrate/20190226074324_remove_old_columns.rb
+++ db/migrate/20190226074324_remove_old_columns.rb
@@ -5,7 +5,6 @@ class RemoveOldColumns < ActiveRecord::Migration[5.2]
     remove_column :articles, :categories
     remove_column :articles, :wp_id
     remove_column :articles, :status
-    remove_column :articles, :structured_content
 
     remove_column :artworks, :wp_id
     remove_column :artworks, :wp_file
