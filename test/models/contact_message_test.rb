432c9e1d2cee8544db7cfd35f9c285718b719c91
Brian Flanagan <brian@elbow.ie>
2019-04-09 23:15:31 +0100

    fix ebun thing

--- test/models/contact_message_test.rb
+++ test/models/contact_message_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class ContactMessageTest < ActiveSupport::TestCase
-  test "marked as spam" do
-    contact_message = build(:contact_message, user: nil)
+  test "is anonymous" do
+    contact_message = build(:contact_message, full_name: nil, email_address: nil)
     assert contact_message.is_anonymous?
+
+    contact_message = build(:contact_message, full_name: Faker::Name.name, email_address: nil)
+    assert !contact_message.is_anonymous?
   end
 end
