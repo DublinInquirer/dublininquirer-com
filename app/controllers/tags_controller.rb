a8955b5d34b1eb53ba3650095a0ada6ffffc6d51
Brian Flanagan <brian@elbow.ie>
2019-04-09 12:33:23 +0100

    undo tag cache

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,9 +3,7 @@ class TagsController < ApplicationController
     @tag = Tag.find_by(slug: params[:id])
     @page = [params[:p].to_i, 1].compact.max
 
-    @articles = Rails.cache.fetch("/tags/#{ @tag.id }/articles/#{ @page }", expires_in: 1.hour) do
-      Article.in_tag(@tag).published.by_date.page(@page)
-    end
+    @articles = Article.in_tag(@tag).published.by_date.page(@page)
 
     if !@articles.any?
       raise ActiveRecord::RecordNotFound
