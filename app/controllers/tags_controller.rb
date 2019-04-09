cc5216f368cf0de8fe0e5ee0660c9dc7613f88d3
Brian Flanagan <brian@elbow.ie>
2019-04-09 12:23:40 +0100

    ibid

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,9 @@ class TagsController < ApplicationController
     @tag = Tag.find_by(slug: params[:id])
     @page = [params[:p].to_i, 1].compact.max
 
-    @articles = Article.in_tag(@tag).published.by_date.page(@page)
+    @articles = Rails.cache.fetch("/tags/#{ @tag.id }/articles/#{ @page }", expires_in: 1.hour) do
+      Article.in_tag(@tag).published.by_date.page(@page)
+    end
 
     if !@articles.any?
       raise ActiveRecord::RecordNotFound
