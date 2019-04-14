1106171cacb03ca155f7b4a42ce0138b98833640
Brian Flanagan <brian@elbow.ie>
2019-04-14 18:07:56 +0100

    throw 404 if no author exists before going further

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,12 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.attempt_to_find_by_slug(params[:id])
-    @page = [params[:p].to_i, 1].compact.max
-    @articles = @author.articles.published.by_date.page(@page)
 
     if !@author
       raise ActiveRecord::RecordNotFound
     end
+
+    @page = [params[:p].to_i, 1].compact.max
+    @articles = @author.articles.published.by_date.page(@page)
   end
 end
