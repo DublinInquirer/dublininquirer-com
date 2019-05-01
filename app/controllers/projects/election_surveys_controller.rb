9fe5e924d4c95cf709ee6879d650c306a6c864e2
Brian Flanagan <brian@elbow.ie>
2019-05-01 11:45:30 +0100

    more low-level controller caching for parsing survey data

--- app/controllers/projects/election_surveys_controller.rb
+++ app/controllers/projects/election_surveys_controller.rb
@@ -3,38 +3,81 @@ class Projects::ElectionSurveysController < ApplicationController
 
   def show
     @survey = ElectionSurvey.find_by!(slug: params[:id])
-    @areas = @survey.area_objects
-    @parties = @survey.party_objects
-    @candidates = @survey.candidate_objects
-    @blue = true
+    @areas = survey_cache(@survey, "areas") do
+      @survey.area_objects
+    end
+    @parties = survey_cache(@survey, "parties") do
+      @survey.party_objects
+    end
+    @candidates = survey_cache(@survey, "candidates") do
+      @survey.candidate_objects
+    end
   end
 
   def area
     @survey = ElectionSurvey.find_by!(slug: params[:id])
-    @areas = @survey.area_objects
-    @area = @survey.find_area(params[:area_id])
-    @candidates = @survey.candidates_for_area(@area)
-    @nonresponders = ElectionSurvey.filter_nonresponders(@survey, @candidates)
-    @questions = @survey.question_objects
-    @blue = true
+
+    @areas = survey_cache(@survey, "areas") do
+      @survey.area_objects
+    end
+    @area = survey_cache(@survey, "areas/#{ params[:area_id] }") do
+      @survey.find_area(params[:area_id])
+    end
+    @candidates = survey_cache(@survey, "areas/#{ @area.slug }/candidates") do
+      @survey.candidates_for_area(@area)
+    end
+    @nonresponders = survey_cache(@survey, "nonresponders") do
+      ElectionSurvey.filter_nonresponders(@survey, @candidates)
+    end
+    @questions = survey_cache(@survey, "questions") do
+      @survey.question_objects
+    end
   end
 
   def party
     @survey = ElectionSurvey.find_by!(slug: params[:id])
-    @parties = @survey.party_objects
-    @party = @survey.find_party(params[:party_id])
-    @candidates = @survey.candidates_for_party(@party)
-    @nonresponders = ElectionSurvey.filter_nonresponders(@survey, @candidates)
-    @questions = @survey.question_objects
-    @blue = true
+
+    @parties = survey_cache(@survey, "parties") do
+      @survey.party_objects
+    end
+    @party = survey_cache(@survey, "parties/#{ params[:party_id] }") do
+      @survey.find_party(params[:party_id])
+    end
+    @candidates = survey_cache(@survey, "parties/#{ @party.slug }/candidates") do
+      @survey.candidates_for_party(@party)
+    end
+    @nonresponders = survey_cache(@survey, "nonresponders") do
+      ElectionSurvey.filter_nonresponders(@survey, @candidates)
+    end
+    @questions = survey_cache(@survey, "questions") do
+      @survey.question_objects
+    end
   end
 
   def candidate
     @survey = ElectionSurvey.find_by!(slug: params[:id])
-    @candidate = @survey.find_candidate(params[:candidate_id])
-    @questions = @survey.question_objects
-    @party = @survey.find_party(@candidate.party)
-    @area = @survey.find_area(@candidate.area)
-    @blue = true
+
+    @candidate = survey_cache(@survey, "candidates/#{ params[:candidate_id] }") do
+      @survey.find_candidate(params[:candidate_id])
+    end
+    @questions = survey_cache(@survey, "questions") do
+      @survey.question_objects
+    end
+    @party = survey_cache(@survey, "parties/#{ @candidate.party }") do
+      @survey.find_party(@candidate.party)
+    end
+    @area = survey_cache(@survey, "areas/#{ @candidate.area }") do
+      @survey.find_area(@candidate.area)
+    end
+  end
+
+  private
+
+  def survey_cache(survey, key)
+    Rails.cache.fetch([cache_key(survey), key].join('/')) { yield }
+  end
+
+  def cache_key(survey)
+    @cache_key ||= [survey.id, survey.updated_at].join('-')
   end
 end
