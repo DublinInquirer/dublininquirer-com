b42992cd48e1cef982401d3ad47351434011664a
Brian Flanagan <brian@elbow.ie>
2019-07-26 07:44:24 +0100

    more actionable dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,7 +4,7 @@ class Admin::DashboardController < Admin::ApplicationController
   def show
     @current_issue = Issue.current
     @recent_subs = Subscription.where(status: 'active').order('created_at desc').limit(10)
-    @recent_churns = Subscription.churned.order('ended_at desc').limit(10)
+    @in_danger = Subscription.where(status: 'past_due').order('created_at asc').limit(10)
     @shame_mrr = (User.where(sources_count: 0).joins(:subscriptions).merge(Subscription.where(status: 'past_due')).map { |u| u.subscription.mrr }.compact.sum / 100.0)
   end
 end
