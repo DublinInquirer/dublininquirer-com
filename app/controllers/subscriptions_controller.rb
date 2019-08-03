132c39e25299afb3867872846b0b0e1f1c20ca9b
Brian Flanagan <brian@elbow.ie>
2019-08-03 16:38:02 +0100

    rename landing page set

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -31,7 +31,7 @@ class SubscriptionsController < ApplicationController
     end
 
     if @subscription.requires_address?
-      @subscription.landing_page = nil
+      @subscription.landing_page_slug = nil
       @user.address_line_1 = @subscription.address_line_1
       @user.address_line_2 = @subscription.address_line_2
       @user.city = @subscription.city
