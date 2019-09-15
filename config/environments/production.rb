795e2ac44eabe1a1ff6e262c8d4ef501eb424b23
Brian Flanagan <brian@elbow.ie>
2019-09-15 09:25:38 +0100

    re-enable asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  # config.action_controller.asset_host = Rails.application.credentials.dig(:production, :aws, :asset_cloudfront_url)
+  config.action_controller.asset_host = Rails.application.credentials.dig(:production, :aws, :asset_cloudfront_url)
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
