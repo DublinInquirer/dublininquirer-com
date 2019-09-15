9304794ecb9208e3c4108a08e298ce89873c1c7d
Brian Flanagan <brian@elbow.ie>
2019-09-15 17:17:18 +0100

    setup stripe webhooks but don't do anything with them for now

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -20,13 +20,4 @@ else
 end
 
 StripeEvent.configure do |events|
-  events.subscribe 'charge.failed' do |event|
-    event.class       #=> Stripe::Event
-    event.type        #=> "charge.failed"
-    event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
-  end
-
-  events.all do |event|
-    # Handle all event types - logging, etc.
-  end
 end
