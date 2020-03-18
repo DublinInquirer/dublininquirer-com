bcfb4c3ff4490dba4ab8b8ace1baa4b6239bd645
brian flanagan <brian@elbow.ie>
2020-03-18 00:20:54 +0000

    really janky suggestion form

--- app/controllers/projects/neighbours/services_controller.rb
+++ app/controllers/projects/neighbours/services_controller.rb
@@ -9,10 +9,31 @@ class Projects::Neighbours::ServicesController < ApplicationController
   end
 
   def new
+    @errors = {}
+    @service = {}
   end
 
   def create
-    raise params.inspect
+    payload = {
+      organisation: params[:organisation],
+      description: params[:description],
+      contact_name: params[:contact_name],
+      contact_email: params[:contact_email],
+      contact_number: params[:contact_number],
+      contact_url: params[:contact_url],
+      address_line_1: params[:address_line_1],
+      address_line_2: params[:address_line_2],
+      address_city: params[:address_city],
+      address_county: params[:address_county],
+      address_postcode: params[:address_postcode]
+    }
+    response = create_service(payload)
+    if response.status.success?
+      redirect_to [:projects, :neighbours, :services]
+    else
+      @errors = Oj.load(response.body)['errors']
+      render :new
+    end
   end
 
   private
@@ -25,11 +46,14 @@ class Projects::Neighbours::ServicesController < ApplicationController
   end
 
   def get_services
-    url = Rails.env.production? ? "https://helpers.civictech.ie/api/services.json" : "http://0.0.0.0:5001/api/services.json"
     begin
       Oj.load(HTTP.get("https://helpers.civictech.ie/api/services.json").body)
     rescue HTTP::ConnectionError, OpenSSL::SSL::SSLError, NoMethodError
       []
     end
+  end
+
+  def create_service(payload)
+    HTTP.post('http://0.0.0.0:5001/api/services.json', json: {service: payload})
   end
 end
