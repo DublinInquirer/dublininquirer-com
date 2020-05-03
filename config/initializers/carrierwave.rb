11740d2378c59641a11b9b8ffef1068e4970f83a
brian flanagan <brian@elbow.ie>
2020-05-03 10:07:45 +0100

    use linode obj storage rather than s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,14 +9,16 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :aws, :secret_access_key),
-      region: 'eu-west-1'
+      aws_access_key_id: Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :linode, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :linode, :secret_access_key),
+      endpoint: 'eu-central-1.linodeobjects.com',
+      region: ''
     }
 
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.asset_host = Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :aws, :ugc_cloudfront_url)
-    config.fog_directory  = Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :aws, :bucket)
+    config.fog_directory  = Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :linode, :bucket)
+    config.s3_access_policy = :public_read
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     config.storage = :fog
   end
