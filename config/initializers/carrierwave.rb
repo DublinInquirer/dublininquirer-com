78ea7a56f65d57abb904d0b5987233d379c203ea
brian flanagan <brian@elbow.ie>
2020-05-03 10:46:04 +0100

    rm s3 access policy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,7 +18,6 @@ if Rails.env.production?
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.asset_host = Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :aws, :ugc_cloudfront_url)
     config.fog_directory  = Rails.application.credentials.dig(ENV['PRODUCTION_ENVIRONMENT'].to_sym, :linode, :bucket)
-    config.s3_access_policy = :public_read
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     config.storage = :fog
   end
