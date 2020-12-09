c5a3a23af941a19f8182996f48c465be11b7f6f8
Brian Flanagan <brian@elbow.ie>
2020-12-09 11:15:22 +0000

    cache the internal path generator thingy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,9 @@ class ArticlesController < ApplicationController
 
   def internal_slug(params)
     return unless params[:slug].present?
-    slug = params[:slug].split(/[^\w-]/i)&.first
-    "/#{ params[:year] }/#{ params[:month] }/#{ params[:day] }/#{ params[:slug] }"
+    Rails.cache.fetch("/slugs/#{ params.slice(:year, :month, :day, :slug).to_a.flatten.join('/') }") do
+      slug = params[:slug].split(/[^\w-]/i)&.first
+      "/#{ params[:year] }/#{ params[:month] }/#{ params[:day] }/#{ slug }"
+    end
   end
 end
