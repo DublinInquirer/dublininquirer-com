62ca93e42c91f85138c3a871ba2a1fbb90aa62be
Brian Flanagan <brian@elbow.ie>
2021-03-16 21:45:59 +0000

    use stimulus for cookies n stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,13 +12,15 @@
 // or the `imagePath` JavaScript helper below.
 //
 
+import Rails from "@rails/ujs";
 import * as Turbo from "@hotwired/turbo";
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 const images = require.context("../images", true);
 const imagePath = (name) => images(name, true);
 
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
+Rails.start();
 
 const application = Application.start();
 const context = require.context("../controllers", true, /\.js$/);
