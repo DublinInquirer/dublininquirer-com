791cefe0163b6f23130e10c9598515604b6dc04b
Brian Flanagan <brian@elbow.ie>
2021-12-17 23:37:01 +0000

    potential asset fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,3 +3,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'javascripts')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf admin.css admin.js fonts.css audio.css elections.css elections.js neighbours.css)
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
