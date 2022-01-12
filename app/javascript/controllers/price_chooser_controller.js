fbb34a5548f3dd70b730456829dae75a108aaf5b
Brian Flanagan <brian@elbow.ie>
2022-01-12 23:47:37 +0000

    do better error handling around price chooser form

--- app/javascript/controllers/price_chooser_controller.js
+++ app/javascript/controllers/price_chooser_controller.js
@@ -2,21 +2,42 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="price-chooser"
 export default class extends Controller {
-  static targets = ['amount', 'productSlug'];
+  static targets = ['amount', 'productSlug', 'subscribeButton'];
 
   connect() {
     this.element.action = '';
   }
 
   validateAmount() {
-    const min = this.minimumFor(this.productSlugTarget.value);
-    console.log(min);
-    // TODO!
+    this.amountTarget.value = parseInt(this.amountTarget.value);
+    if (this.amountTarget.value < this.minimumFor(this.productSlugTarget.value)) {
+      this.addError();
+    } else {
+      this.removeError();
+    }
+  }
+
+  addError() {
+    this.amountTarget.parentNode.parentNode.classList.add('-error');
+    this.amountTarget.parentNode.parentNode.querySelector(
+      '.hint',
+    ).innerHTML = `Minimum €${this.minimumFor(this.productSlugTarget.value)}/month`;
+  }
+
+  removeError() {
+    this.amountTarget.parentNode.parentNode.classList.remove('-error');
+    this.amountTarget.parentNode.parentNode.querySelector('.hint').innerHTML = 'per month';
   }
 
   subscribe(e) {
+    this.disableSubmit();
     e.preventDefault();
-    window.location.href = `/v2/subscribe/${this.productSlugTarget.value}/${this.amountTarget.value}`;
+    if (this.anyErrors()) {
+      this.undisableSubmit();
+      this.shake();
+    } else {
+      window.location.href = `/v2/subscribe/${this.productSlugTarget.value}/${this.amountTarget.value}`;
+    }
   }
 
   minimumFor(slug) {
@@ -26,5 +47,24 @@ export default class extends Controller {
       case 'print':
         return 9;
     }
+  }
+
+  anyErrors() {
+    return this.amountTarget.parentNode.parentNode.classList.contains('-error');
+  }
+
+  shake() {
+    this.amountTarget.parentNode.parentNode.style.animation = 'none';
+    window.requestAnimationFrame(() => {
+      this.amountTarget.parentNode.parentNode.style.animation =
+        'shake 0.76s cubic-bezier(0.36, 0.07, 0.19, 0.97) both';
+    });
+  }
+
+  disableSubmit() {
+    this.subscribeButtonTarget.disabled = true;
+  }
+  undisableSubmit() {
+    this.subscribeButtonTarget.disabled = false;
   }
 }
